24b6c47d4be4d57bb7410575125d2e423835cd36
YaoPaine <yaohe1991@gmail.com>
2017-02-16 16:55:57 +0800

    delete three files

--- day01.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	int i;//sheng ming yi ge int lei xing de bian liang
-	printf("hello world!");
-	return 0;
-}
--- day02.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int i=100;
-	printf("how old are you? %d",i);
-	return 0;
-}
